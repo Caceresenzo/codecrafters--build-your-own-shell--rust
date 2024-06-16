37db5f7ad9fd4d49b0442d43325fd8d87bfc104a
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-16 19:12:13 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    let stdin = io::stdin();
+    let stdin: io::Stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let program = &input[..input.len() - 1];
+    println!("{}: command not found", program);
 }
