dd3dda11eff299985a8b70902c69b8d860f3e6d4
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-08 01:39:02 +0100

    CLI tests (cli-test-1733618342257)

---
+++ src/command.rs
@@ -0,0 +1,30 @@
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+use crate::{BuiltinFunction, REGISTRY};
+
+pub enum ShellCommand {
+    Builtin(BuiltinFunction),
+    Executable(PathBuf),
+    None,
+}
+
+pub fn query(program: &str) -> ShellCommand {
+    if let Some(builtin) = REGISTRY.read().unwrap().get(program) {
+        return ShellCommand::Builtin(*builtin);
+    }
+
+    if let Ok(paths) = env::var("PATH") {
+        for directory in paths.split(":") {
+            let path = Path::new(directory).join(program);
+
+            if path.exists() {
+                return ShellCommand::Executable(path);
+            }
+        }
+    }
+
+    return ShellCommand::None;
+}
