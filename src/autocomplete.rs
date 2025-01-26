ead619331d1f54b402dd57ed79d0e47c296f62b8
Caceresenzo <caceresenzo1502@gmail.com>
2025-01-26 18:12:03 +0100

    pass 1st autocompletion

---
+++ src/autocomplete.rs
@@ -0,0 +1,43 @@
+use std::{
+    io::{self, Write},
+    vec::Vec,
+};
+
+use crate::REGISTRY;
+
+pub enum AutocompleteResult {
+    None,
+    Found,
+    More,
+}
+
+fn commit(line: &mut String, candidate: &String) {
+    line.push_str(candidate);
+    io::stdout().write(candidate.as_bytes()).unwrap();
+
+    line.push(' ');
+    io::stdout().write(&[b' ']).unwrap();
+
+    io::stdout().flush().unwrap();
+}
+
+pub fn autocomplete(line: &mut String) -> AutocompleteResult {
+    let mut candidates: Vec<String> = Vec::new();
+
+    for key in REGISTRY.read().unwrap().keys() {
+        if key.starts_with(&*line) {
+            let candidate = &key[line.len()..];
+            candidates.push(candidate.to_string());
+        }
+    }
+
+    if candidates.is_empty() {
+        return AutocompleteResult::None;
+    } else if candidates.len() == 1 {
+        commit(line, &candidates[0]);
+        return AutocompleteResult::Found;
+    }
+
+    todo!("more");
+    // return AutocompleteResult::More;
+}
